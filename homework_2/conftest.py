e77befc5fb0e90341843889d7fa049e77d8f80cd
CMX\kplyashechnik <kplyashechnik@cinimex.ru>
2020-10-08 19:18:27 +0300

    Задание 2. API dog_api - done

---
+++ homework_2/conftest.py
@@ -0,0 +1,25 @@
+import pytest
+import requests
+
+
+class ApiClient:
+    def __init__(self, base_address):
+        self.base_address = base_address
+
+    def get(self, path="/", params=None):
+        url = self.base_address + path
+        return requests.get(url=url, params=params)
+
+
+@pytest.fixture(scope="session")
+def url_api_dog(request):
+    return ApiClient(base_address="https://dog.ceo/api/")
+
+
+@pytest.fixture()
+def schema_dog_json():
+    schema = {
+        "message": {"type": "array"},
+        "status": {"type": "string"}
+    }
+    return schema
---
+++ homework_2/dog_api_test.py
@@ -0,0 +1,35 @@
+import requests
+import pytest
+from jsonschema import validate
+
+
+def test_status(url_api_dog):
+    r = url_api_dog.get("breeds/list/all")
+    r_status = r.json()['status']
+    assert r_status == 'success'
+
+
+@pytest.mark.parametrize("test_input", [1, 5, 13, 49])
+def test_img_count(test_input, url_api_dog):
+    random_count = test_input
+    r = url_api_dog.get(f"breeds/image/random/{random_count}")
+    count_object = len(r.json()['message'])
+    assert test_input == count_object
+
+
+def test_breed_australian(url_api_dog):
+    r = url_api_dog.get("breed/australian/list")
+    dog = r.json()['message']
+    assert dog == ['shepherd']
+
+
+@pytest.mark.parametrize("test_input", ['hound', 'mountain', 'setter', 'spaniel'])
+def test_list_breed(test_input, schema_dog_json, url_api_dog):
+    r = url_api_dog.get("breed/{test_input}/list")
+    validate(instance=r.json(), schema=schema_dog_json)
+
+
+def test_img_random(url_api_dog):
+    r = url_api_dog.get("breed/beagle/images/random")
+    beagle_dog = r.json()['status']
+    assert beagle_dog == 'success'
